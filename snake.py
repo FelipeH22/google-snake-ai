b77bb8308c2f1b52f1d18c08536a5db2d812b037
Juan Felipe Herrera <42241671+FelipeH22@users.noreply.github.com>
2023-03-15 12:55:35 -0500

    The initialization process (enter and right button) is added (#2)

--- snake.py
+++ snake.py
@@ -1,14 +1,20 @@
 from PIL import ImageGrab
 import numpy as np
+import pyautogui as pkey
+import time
 import cv2
 
 def get_screen():
+    #Initialize the game by pressing enter and moving the snake to the right
+    time.sleep(3)
+    pkey.keyDown('Enter')
+    pkey.keyDown('D')
     while True:
         org=cv2.cvtColor(np.array(ImageGrab.grab(bbox=(0,230,900,1000))),cv2.COLOR_BGR2RGB)
         grey_image=cv2.Canny(org,20,30)
         cv2.imshow('window',grey_image)
-        if cv2.waitKey(25) & 0xff == ord('q'):
+        if cv2.waitKey(25) & 0xff == ord('q'):  
             cv2.destroyAllWindows()
             break
-
+        
 get_screen()
