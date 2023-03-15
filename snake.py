a97230201b5cbd0cd7ce7b18b12b93f315ba923d
FelipeH22 <juan22herrera01@gmail.com>
2023-03-15 18:51:38 -0500

    Detection of internal lines (i.e. the 'matrix' where the snake can move) and the countour of the board. However, the left border is not recognized yet because the function creates a segmented line. Further changes must be applied to get the borders right

--- snake.py
+++ snake.py
@@ -5,16 +5,26 @@ import time
 import cv2
 
 def get_screen():
-    #Initialize the game by pressing enter and moving the snake to the right
     time.sleep(3)
-    pkey.keyDown('Enter')
     pkey.keyDown('D')
     while True:
         org=cv2.cvtColor(np.array(ImageGrab.grab(bbox=(0,230,900,1000))),cv2.COLOR_BGR2RGB)
         grey_image=cv2.Canny(org,20,30)
-        cv2.imshow('window',grey_image)
+        edges=cv2.HoughLinesP(grey_image,1,np.pi/180,threshold=100,minLineLength=.5,maxLineGap=100)
+        external_edges=cv2.HoughLinesP(grey_image,1,np.pi/180,threshold=30,minLineLength=50,maxLineGap=.0001)
+        draw_edges(org,edges,1)
+        draw_edges(org,external_edges,0)
+        cv2.imshow('original',org)
         if cv2.waitKey(25) & 0xff == ord('q'):  
             cv2.destroyAllWindows()
             break
+
+def draw_edges(img,edges,pos):
+    for point in edges:
+        x1,y1,x2,y2=point[0]
+        if pos==1:
+            cv2.line(img,(x1,y1),(x2,y2),(0,255,0),2)
+        else:
+            cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
         
 get_screen()
