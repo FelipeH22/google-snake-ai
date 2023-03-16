025685695f7ba785b05bf1afbc3efb55445ac946
FelipeH22 <juan22herrera01@gmail.com>
2023-03-16 09:01:18 -0500

    Contours and circle (apple) detection is added. An improvement should be done because the apple sometimes gets confused with the border or with the snake when it appears close to one of them

--- snake.py
+++ snake.py
@@ -9,23 +9,41 @@ def get_screen():
     pkey.keyDown('D')
     while True:
         org=cv2.cvtColor(np.array(ImageGrab.grab(bbox=(0,230,900,1000))),cv2.COLOR_BGR2RGB)
+        
+        #Get the grid
         grey_image=cv2.Canny(org,20,30)
         edges=cv2.HoughLinesP(grey_image,1,np.pi/180,threshold=100,minLineLength=.5,maxLineGap=100)
-        external_edges=cv2.HoughLinesP(grey_image,1,np.pi/180,threshold=30,minLineLength=50,maxLineGap=.0001)
-        draw_edges(org,edges,1)
-        draw_edges(org,external_edges,0)
+
+        #Get apple
+        apple=cv2.Canny(org,200,400)
+        apple_cont=cv2.HoughCircles(apple,cv2.HOUGH_GRADIENT,4,100,minRadius=20,maxRadius=25)
+
+        #Get contour of the snake
+        snake=cv2.Canny(org,0,680)
+        snake_contour,_=cv2.findContours(snake,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+
+        #Get contour of the board
+        board=cv2.Canny(org,100,150)
+        board_contour,_=cv2.findContours(board,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+        
+        #Draw
+        cv2.drawContours(org,board_contour,-1,0,3)
+        draw_circles(org,apple_cont)
+        cv2.drawContours(org,snake_contour,-1,(0,0,255),3)
         cv2.imshow('original',org)
-        cv2.imshow('edges',grey_image)
         if cv2.waitKey(25) & 0xff == ord('q'):  
             cv2.destroyAllWindows()
             break
 
-def draw_edges(img,edges,pos):
+def draw_edges(img,edges):
     for point in edges:
         x1,y1,x2,y2=point[0]
-        if pos==1:
-            cv2.line(img,(x1,y1),(x2,y2),(255,0,0),2)
-        else:
-            cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
+        cv2.line(img,(x1,y1),(x2,y2),(255,0,0),2)
+
+def draw_circles(img,circles):
+    if circles is not None:
+        circles=np.round(circles[0,:]).astype('int')
+        for (x,y,r) in circles:
+            cv2.circle(img,(x,y),r,(50,205,50),3)
         
 get_screen()
