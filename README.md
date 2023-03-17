d36779f230b050736eb1974bf0c3d0b873a317ae
Juan Felipe Herrera <42241671+FelipeH22@users.noreply.github.com>
2023-03-17 14:40:03 -0500

    Feature/pos extraction (#7)
    * Some detection techniques changed. Also, the most naive way of moving using the contours that I could came up with is added
    
    * Update README.md
    
    * The detection method changed. It is now more effective so the detection of the eyes does not affect the mean of the head's position
    
    * The position extraction is finally done right. The contours do not get confused with each other anymore. YEEESS!!!. I think it is time to implement the A*
    
    * Some minor changes

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # google_snake_ai
-Implementation of an algorithm that plays the google snake game.
+Implementation of an algorithm (hopefully, a good implementation of the A* algorithm) that plays the google snake game using the contours detected.
--- snake.py
+++ snake.py
@@ -5,12 +5,12 @@ import time
 import cv2
 
 def screen():
-    time.sleep(3)
-    #pkey.keyDown('D')
+    time.sleep(2)
     while True:
         org=cv2.cvtColor(np.array(ImageGrab.grab(bbox=(0,230,900,1000))),cv2.COLOR_BGR2RGB)
         final_img,board,snake_body,snake_head,apple_cont=get_edges(org)
-        get_positions(snake_head,apple_cont)
+        get_positions(snake_head,apple_cont,board,snake_body)
+
         #Draw
         cv2.imshow('Processed Image',final_img)
         if cv2.waitKey(25) & 0xff == ord('q'):  
@@ -18,23 +18,36 @@ def screen():
             break
 
 def get_edges(original):
-    #Get apple and snake head contour
-    apple=cv2.Canny(original,200,400)
-    apple_cont=cv2.HoughCircles(apple,cv2.HOUGH_GRADIENT,4,100,minRadius=23,maxRadius=26)
-    snake_head=cv2.HoughCircles(apple,cv2.HOUGH_GRADIENT,8,0.00001,maxRadius=12)
 
     #Get contour of the snake body
-    snake=cv2.Canny(original,0,680)
-    snake_contour,_=cv2.findContours(snake,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+    original=cv2.cvtColor(original,cv2.COLOR_BGR2HSV)
+    mask=cv2.inRange(original,np.array([110,50,50]),np.array([130,255,255]))
+    body=cv2.bitwise_and(original,original,mask=mask)
+    body=cv2.Canny(body,20,100)
+    snake_contour,_=cv2.findContours(body,cv2.RETR_CCOMP,cv2.CHAIN_APPROX_SIMPLE)
+
+    #Get apple contour
+    mask=cv2.inRange(original,np.array([-10,100,100]),np.array([10,225,255]))
+    apple=cv2.bitwise_and(original,original,mask=mask)
+    apple=cv2.Canny(apple,20,100)
+    apple_cont,_=cv2.findContours(apple,cv2.RETR_CCOMP,cv2.CHAIN_APPROX_SIMPLE)
+    
+    #Get contour of the eyes
+    mask=cv2.inRange(original,np.array([0,0,255-15]),np.array([255,15,255]))
+    head=cv2.bitwise_and(original,original,mask=mask)
+    head=cv2.Canny(head,0,100)
+    snake_head,_=cv2.findContours(head,cv2.RETR_CCOMP,cv2.CHAIN_APPROX_SIMPLE)
 
     #Get contour of the board
-    board=cv2.Canny(original,100,150)
-    board_contour,_=cv2.findContours(board,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    board=cv2.Canny(original,100,300)
+    board=cv2.GaussianBlur(board,(5,5),7)
+    board_contour,_=cv2.findContours(board,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
 
     #Draw
+    original=cv2.cvtColor(original,cv2.COLOR_HSV2RGB)
     cv2.drawContours(original,board_contour,-1,0,3)
-    draw_circles(original,apple_cont,1)
-    draw_circles(original,snake_head,0)
+    cv2.drawContours(original,apple_cont,-1,(50,205,50),3)
+    cv2.drawContours(original,snake_head,-1,(176,224,230),3)
     cv2.drawContours(original,snake_contour,-1,(0,0,255),3)
 
     return original,board_contour,snake_contour,snake_head,apple_cont
@@ -49,10 +62,28 @@ def draw_circles(img,circles,t):
             else:
                 cv2.circle(img,(x,y),r,(255,0,0),3)
 
-def get_positions(snake,fruit):
-    if snake is not None:
-        snake_pos=np.mean(snake[0],axis=0)[:-1]
-    if fruit is not None:
-        fruit_pos=fruit[0][0][:-1]
+def get_positions(snake,fruit,board,snake_b):
+    snake_pos=get_relative_position(snake)
+    fruit_pos=get_relative_position(fruit)
+    if type(snake_pos) is np.ndarray and type(fruit_pos) is np.ndarray:#40
+        if fruit_pos[0]-snake_pos[0]>40: pkey.keyDown("D")
+        if fruit_pos[0]-snake_pos[0]<-40: pkey.keyDown("A")
+        if fruit_pos[1]-snake_pos[1]<-40: pkey.keyDown("W")
+        if fruit_pos[1]-snake_pos[1]>40: pkey.keyDown("S")
+
+def get_relative_position(contour):
+    if contour is not None:
+        pos=list()
+        for i in contour:
+            M=cv2.moments(i)
+            if M['m00']!=0:
+                x=int(M['m10']/M['m00'])
+                y=int(M['m01']/M['m00'])
+                if x>=0 and y>=0:
+                    pos.append([x,y])
+        pos=np.nanmean(np.asarray(pos),axis=0)
+        return pos
+    return np.nan
+    
         
 screen()
